bf363bbcd5a4a73c3e07f2eddc18a23257f3bd5d
pelx <laura.peppiatt@btinternet.com>
2021-03-13 19:12:41 +0000

    Deploy to netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
+const colors = require("./node_modules/tailwindcss/colors");
+
 module.exports = {
-  purge: ['./src/**/*.{html,ts}'],
+  purge: [
+    "./src/**/*.html",
+    "./src/**/*.ts",
+    "./src/**/*.css",
+    "./src/**/*.scss",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+        orange: colors.orange,
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/line-clamp')
-,require('@tailwindcss/typography')
-],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
+
+// module.import { colors } from "@tailwindcss/colors";
